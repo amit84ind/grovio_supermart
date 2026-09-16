ee7bdb60d15d6ce915f553975129ab7d20948301
amit84ind <amit84ind@gmail.com>
2026-09-16 09:22:08 +0530

    Fix geocoding dependency 3.0.0 and upgrade Kotlin version to 2.3.20

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -22,7 +22,7 @@ plugins {
     id("com.android.application") version "9.1.1" apply false
     id("com.android.library") version "9.1.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
 }
 
 include(":app")
